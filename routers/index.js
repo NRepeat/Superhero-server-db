ad1df88765bcfa981eb24ff4676d26c0cf4b1610
Nikita <nnazarov55@gmail.com>
2023-08-09 01:17:37 +0300

    add userController

--- routers/index.js
+++ routers/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const rootRouter = express.Router();
 const superheroRouter = require('./superheroRouter');
 const superpowerRouter = require('./superpowerRouter');
-const superheroImgRouter  =require("./superHeroImgRouter")
+const superheroImgRouter  =require("./superHeroImgRouter");
+const userRouter = require('./userRouter');
 
 rootRouter.use('/superhero', superheroRouter);
 rootRouter.use('/superpower',  superpowerRouter);
 rootRouter.use("/superheroImg",superheroImgRouter)
+rootRouter.use("/user",userRouter)
 
 module.exports = rootRouter;
