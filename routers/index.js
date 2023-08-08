4e62cec4675eae08210fb62d1dbf8d8599437713
Nikita <nnazarov55@gmail.com>
2023-08-09 00:49:04 +0300

    fix yup validator mv

--- routers/index.js
+++ routers/index.js
@@ -3,12 +3,9 @@ const rootRouter = express.Router();
 const superheroRouter = require('./superheroRouter');
 const superpowerRouter = require('./superpowerRouter');
 const superheroImgRouter  =require("./superHeroImgRouter")
-const {
-    validateSuperheroBody,
-    validateSuperpowerBody
-  } = require('../middlewares/validate.mv');
+
 rootRouter.use('/superhero', superheroRouter);
-rootRouter.use('/superpower',  validateSuperpowerBody, superpowerRouter);
+rootRouter.use('/superpower',  superpowerRouter);
 rootRouter.use("/superheroImg",superheroImgRouter)
 
 module.exports = rootRouter;
--- routers/superheroRouter.js
+++ routers/superheroRouter.js
@@ -1,14 +1,17 @@
 const superheroRouter = require("express").Router();
 
 const superheroController = require("../controllers/superheroController");
-
+const {
+  validateSuperheroBody,
+  validateSuperpowerBody
+} = require('../middlewares/validate.mv');
 superheroRouter.route("/all").get(superheroController.getAllSuperheros);
 
 superheroRouter.route("/").post(superheroController.createSuperhero);
 superheroRouter
   .route("/:superheroId")
   .get(superheroController.getSuperhero)
-  .put(superheroController.updateSuperhero)
+  .put(validateSuperheroBody,superheroController.updateSuperhero)
   .delete(superheroController.deleteSuperhero);
 
 module.exports = superheroRouter;
